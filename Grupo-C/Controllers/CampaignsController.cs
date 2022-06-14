b45449e6e9ec03bedbe6e7136a3fd243b16af0d5
Douke017 <Douglas.mendez@ucb.edu.bo>
2022-06-14 17:30:09 -0400

    Final Changes

--- Grupo-C/Controllers/CampaignsController.cs
+++ Grupo-C/Controllers/CampaignsController.cs
@@ -1,10 +1,6 @@
 ﻿using Logic.Managers;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 
 namespace Grupo_C.Controllers
